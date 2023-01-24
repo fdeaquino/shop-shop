66f7901c8de59b98688e8fef532a5ae91e76b8d3
FDeaquino <fidel.deaquino@gmail.com>
2023-01-24 12:48:40 -0600

    wrote more tests for Shopping Cart Reducers, created layout for shopping cart, implemented Cart toggle, Implemented Adding items to the Cart, Implemented Updating and Delting Items

--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -6,3 +6,11 @@ export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 
 //UPDATE_CURRENT_CATEGORY is the connecting piece of data from the prev two actions - we want to be able to select a category from the state created by UPDATE_CATEGORIES action, and display products form that category from the list we create from the UPDATE_PRODUCTS action
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
+// These actions are the ones we might weant to perform with a shopping cart..
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const CLEAR_CART = 'CLEAR_CART';
+export const TOGGLE_CART = 'TOGGLE_CART';
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -3,7 +3,13 @@ import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
 } from "./actions";
 
 export const reducer = (state, action) => {
@@ -27,6 +33,61 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 currentCategory: action.currentCategory
+            };
+
+        // if action type value iss the value of `ADD_TO_CART`, return the updated cart property to add "action.product" to the end of the array, we also set cartOpen to true so users can immediately view the cart with the newly added item
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            };
+
+        // if action type value iss the value of `ADD_MULTIPLE_TO_CART`, return the updated cart property to add "action.products" to the end of the array, we also set cartOpen to true so users can immediately view the cart with the newly added item
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
+            };
+
+        // The use of the "filter()" method that only keeps the items that don't match the provided _id property. In the return statement, we also check the length of the array to set cartOpen to false when the array is empty. 
+        case REMOVE_FROM_CART:
+            let newState = state.cart.filter(product => {
+                return product._id !== action._id;
+            });
+
+            return {
+                ...state,
+                cartOpen: newState.length > 0,
+                cart: newState
+            };
+
+        // Note: we used the "map()" method to create a new array instead of updating "state.cart" directly because the original state should be treated as immutable.  
+        case UPDATE_CART_QUANTITY:
+            return {
+                ...state,
+                cartOpen: true,
+                cart: state.cart.map(product => {
+                    if (action._id === product._id) {
+                        product.purchaseQuantity = action.purchaseQuantity;
+                    }
+                    return product;
+                })
+            };
+
+        // The cart is empty and closed.
+        case CLEAR_CART:
+            return {
+                ...state,
+                cartOpen: false,
+                cart: []
+            };
+
+        // expets the opposite of its previous value each time the action is called
+        case TOGGLE_CART:
+            return {
+                ...state,
+                cartOpen: !state.cartOpen
             };
 
         // if it's none of these actions, do not update state at all and keep things the same!
